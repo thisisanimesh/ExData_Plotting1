1cf18c7e5a4a09e9b1d78fc1700f6d5e94056c6e
thisisanimesh <pursueanimesh@gmail.com>
2014-07-13 13:05:47 +0530

    first commit

---
+++ plot2.R
@@ -0,0 +1,10 @@
+source("prepData.R")
+
+makePlot2 <- function() {
+  tbl <- prepData()
+  png(filename = "plot2.png", width = 480, height = 480, units = "px")
+  plot(tbl$DateTime, tbl$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+  dev.off()
+}
+
+makePlot2()
